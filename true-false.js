9e8f2c8dcee21f029207bf670a130477165d0e72
mehedi143038 <millathasan125@gmail.com>
2020-03-03 11:00:10 -0800

    true false added

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//falsy
+//false
+//""
+//0
+//null
+//NaN
+//undefined
+
+
+//truthy
+//" "
+//"0"
+
+name = "";
+if(name){
+    console.log("This is true");
+}
+else{
+    console.log("This is false");
+}
